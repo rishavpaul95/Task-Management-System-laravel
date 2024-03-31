054dc24b7563353de5f2bcf027fef430ba7942eb
Rishav Paul <rishavpaul1995@gmail.com>
2024-04-01 00:25:53 +0530

    companies cascade delete

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -9,6 +9,9 @@ use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Redirect;
 use Illuminate\View\View;
 use App\Models\Categories;
+use App\Models\Company;
+use App\Models\User;
+use Illuminate\Support\Facades\Log;
 
 class ProfileController extends Controller
 {
@@ -52,6 +55,30 @@ class ProfileController extends Controller
 
         $user = $request->user();
 
+
+        // Check if the user is an admin or super-admin
+        if ($user->hasRole('admin') || $user->hasRole('super-admin')) {
+            // Check if there are no other users with the role 'admin' or 'super-admin' in the same company
+            $otherAdmins = User::where('company_id', $user->company_id)
+                ->where('id', '!=', $user->id)
+                ->where(function ($query) {
+                    $query->whereHas('roles', function ($query) {
+                        $query->whereIn('name', ['admin', 'super-admin']);
+                    });
+                })
+                ->count();
+
+            if ($otherAdmins == 0) {
+                // Try to delete the company
+                try {
+                    Company::where('id', $user->company_id)->delete();
+                } catch (\Exception $e) {
+                    // Log the error message
+                    Log::error('Failed to delete company: ' . $e->getMessage());
+                }
+            }
+        }
+
         Auth::logout();
 
         $user->delete();
@@ -62,6 +89,3 @@ class ProfileController extends Controller
         return Redirect::to('/');
     }
 }
-
-
-
