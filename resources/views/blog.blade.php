c5276fd6a9af627872cd01503841cbeda831e43a
Rishav Paul <rishavpaul1995@gmail.com>
2024-05-07 15:27:35 +0530

    blog start

--- resources/views/blog.blade.php
+++ resources/views/blog.blade.php
@@ -19,53 +19,71 @@
 
         <section class="content">
             <div class="container-fluid">
+                <div class="container">
+                    <div class="row">
+                        <!-- Blog entries-->
+                        <div class="col-lg-8">
+                            <!-- Featured blog post-->
+                            <div class="card mb-4">
+                                <a href="#!"><img class="card-img-top" src="https://dummyimage.com/850x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                <div class="card-body">
+                                    <div class="small text-muted">May 1, 2024</div>
+                                    <h2 class="card-title">TaskMan Overview</h2>
+                                    <p class="card-text">Learn how to use TaskMan and its features. TaskMan provides an easy way to manage your...</p>
+                                    <a class="btn btn-primary" href={{url('/blog/post1')}}>Read more →</a>
+                                </div>
+                            </div>
+                            <!-- Nested row for non-featured blog posts-->
+                            <div class="row">
+                                <div class="col-lg-6">
+                                    <!-- Blog post-->
+                                    <div class="card mb-4">
+                                        <a href="#!"><img class="card-img-top" src="https://dummyimage.com/700x350/dee2e6/6c757d.jpg" alt="..." /></a>
+                                        <div class="card-body">
+                                            <div class="small text-muted">May 2, 2024</div>
+                                            <h2 class="card-title h4">Rishav Paul</h2>
+                                            <p class="card-text">Learn about Rishav Paul, the Dev behind TaskMan!</p>
+                                            <a class="btn btn-primary" href={{url('/blog/post2')}}>Read more →</a>
+                                        </div>
+                                    </div>
+                                </div>
 
-
-                <div class="row">
-                    <div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
-                            <img src="./dist/img/photo1.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text"><a href="{{ url('/blog/post1') }}">Article 1</a></p>
                             </div>
                         </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
-                            <img src="./dist/img/photo2.png" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text"><a href="{{ url('/blog/post2') }}"> Article 2</a></p>
+                        <!-- Side widgets-->
+                        <div class="col-lg-4">
+                            <!-- Search widget-->
+                            <div class="card mb-4">
+                                <div class="card-header">Search</div>
+                                <div class="card-body">
+                                    <div class="input-group">
+                                        <input class="form-control" type="text" placeholder="Enter search term..." aria-label="Enter search term..." aria-describedby="button-search" />
+                                        <button class="btn btn-primary" id="button-search" type="button">Go!</button>
+                                    </div>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div class="col-md-4">
-                        <div class="card" style="width: 18rem;">
-                            <img src="./dist/img/photo3.jpg" class="card-img-top" alt="...">
-                            <div class="card-body">
-                                <p class="card-text"><a href="{{ url('/blog/post3') }}"> Article 3</a></p>
+                            <!-- Side widget-->
+                            <div class="card mb-4">
+                                <div class="card-header">Side Widget</div>
+                                <div class="card-body">You can put anything you want inside of these side widgets. They are easy to use, and feature the Bootstrap 5 card component!</div>
                             </div>
                         </div>
                     </div>
                 </div>
-
-
-            </div>
         </section>
-        @push('scripts')
-            <script>
-                @if (session('success'))
-                    toastr.success('{{ session('success') }}', 'Success');
-                @endif
 
-                @if (session('error'))
-                    toastr.error('{{ session('error') }}', 'Error');
-                @endif
-
-                @if (session('status'))
-                    toastr.info('{{ session('status') }}', 'Status');
-                @endif
-            </script>
-        @endpush
     </div>
 @endsection
+
+
+
+
+
+
+
+
+
+
+
+
 
